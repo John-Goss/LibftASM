f48764e4d0770c38987ec76f958310aac9118d12
Jonathan LE QUERE <jle-quer@e3r11p21.42.fr>
2017-11-09 17:26:07 +0100

    [MODIFY] First part done

--- src/ft_puts.s
+++ src/ft_puts.s
@@ -1,40 +1,48 @@
 global _ft_puts
 
+section .data
+string:
+		.newLine db 0xa
+		.nullstring db "(null)"
+		.nullLen equ $ - string.nullstring
+
 section .text
 
 _ft_puts:
 		push rbp
 		mov rbp, rsp
+		cmp rdi, 0x0
+		je null
 		mov rsi, rdi
+		mov rax, 0x2000004 ; Syscall WRITE
 		mov rdi, 0x1 ; STDOUT
 		mov rdx, 0x0 ; String lenght
 
 count:
-		cmp [rsi], byte 0x0
+		cmp [rsi + rdx], byte 0x0
 		je writeString
 		inc rdx
-		inc rsi
 		jmp count
 
 writeString:
 		cmp rdx, 0x0
-		je false
-		sub [rsi], rdx
-		mov rax, 0x2000004 ; WRITE is Syscall
+		je end
 		syscall
 		jmp end
 
-false:
-		mov rax, -1
-		leave
-		ret
+null:
+		mov rax, 0x2000004 ; Syscall WRITE
+		mov rdi, 0x1 ; STDOUT
+		lea rsi, [rel string.nullstring]
+		mov rdx, string.nullLen ; size of "(null)"
+		syscall
 
 end:
 		mov rax, 0x2000004
-		mov rdx, 0x1
-		mov rsi, 0xa ; print \n
 		mov rdi, 0x1
+		lea rsi, [rel string.newLine]
+		mov rdx, 0x1
 		syscall
-		mov rax, 0x1
+		mov rax, 0xa
 		leave
 		ret
