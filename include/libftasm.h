5c540d6f842df3d972a9204e3221e810c5c895b4
Jonathan LE QUERE <jle-quer@e3r11p21.42.fr>
2017-11-09 16:00:37 +0100

    [ADD] Include/

---
+++ include/libftasm.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libftasm.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jle-quer <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/11/09 11:07:49 by jle-quer          #+#    #+#             */
+/*   Updated: 2017/11/09 12:30:34 by jle-quer         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFTASM_H
+# define LIBFTASM_H
+
+void		ft_bzero(void *s, size_t n);
+int			ft_isalpha(int c);
+int			ft_islower(int c);
+int			ft_isupper(int c);
+int			ft_isdigit(int c);
+int			ft_isalnum(int c);
+int			ft_isascii(int c);
+int			ft_isprint(int c);
+int			ft_toupper(int c);
+int			ft_tolower(int c);
+int	    	ft_puts(char *s);
+char		*ft_strcat(char *s1, const char *s2);
+
+#endif
