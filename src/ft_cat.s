dfbb056561ebe51ae17769d932f06f81af9f1ff3
Jonathan LE QUERE <jle-quer@e3r8p18.42.fr>
2017-11-15 13:57:48 +0100

    [MODIFY] ft_cat.s - Resolved an compil issue

--- src/ft_cat.s
+++ src/ft_cat.s
@@ -3,11 +3,10 @@ global _ft_cat
 %define SYSCALL(n) 0x2000000 | n
 %define BUFF_SIZE 255
 
-section .bss
-		buffer resb BUFF_SIZE
+section .data
+		buffer: times BUFF_SIZE db 0x0
 
 section .text
-		extern _ft_strlen
 
 _ft_cat:
 		push rbp
@@ -16,15 +15,15 @@ _ft_cat:
 read:
 		push rdi ; Push FD in stack
 		mov rax, SYSCALL(3) ; Read Syscall
-		mov rsi, buffer
+		lea rsi, [rel buffer]
 		mov rdx, BUFF_SIZE
 		syscall
 		jc fail ; Out if err on read (Carry Flag == 1)
 
-		cmp rax, 0
+		cmp rax, 0x0
 		je end ; Exit if EOF
 
-		mov rdi, 1 ; Set STDOUT FD
+		mov rdi, 0x1 ; Set STDOUT FD
 		mov rdx, rax ; Size to Write
 		mov rax, SYSCALL(4) ; Write Syscall
 		syscall
@@ -35,7 +34,7 @@ read:
 
 fail:
 		pop rdi
-		mov rax, 1
+		mov rax, 0x1
 
 end:
 		leave
